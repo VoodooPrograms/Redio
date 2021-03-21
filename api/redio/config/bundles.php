04260758de56de6dd675a26cf8e71e26da114770
Bartosz Belski <bartosz.belski@owlsdepartment.com>
2021-03-21 19:45:46 +0100

    Install lexik_jwt_bundle

--- api/redio/config/bundles.php
+++ api/redio/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
