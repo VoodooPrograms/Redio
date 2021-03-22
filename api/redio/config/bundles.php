426df23ecb8b74f2aff7e1c6c9e287bdce927d7a
Bartosz Belski <bartosz.belski@owlsdepartment.com>
2021-03-22 22:13:28 +0100

    Enable to obtain jwt token from API

--- api/redio/config/bundles.php
+++ api/redio/config/bundles.php
@@ -7,4 +7,7 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
