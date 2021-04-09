cc97b11b9a10330fcb4e27e843da93a07c034b99
Bartosz Belski <bartosz.belski@owlsdepartment.com>
2021-04-09 22:12:36 +0200

    Install and configure Vuex, VueRouter, normalizeCSS

--- ui/ui/src/App.vue
+++ ui/ui/src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+    <router-view />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
+
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0 auto;
+}
+
+main {
+  margin: 0 auto;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+
+:root {
+  --primary-color: #D11828;
+  --grey: #707070;
+  --white: #ffffff;
+  --black: #030303;
+  --bg-color: #0F0F0F;
+}
+
+@font-face {
+  font-family: "Bahnschrift";
+  src: url('./fonts/Bahnschrift/BAHNSCHRIFT.TTF');
+}
+@font-face {
+  font-family: "Segoe UI";
+  src: url('./fonts/segoe-ui/SEGOEUI.TTF');
 }
 </style>
