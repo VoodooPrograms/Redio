247e1f7b2cd3526e94769b3931b323d652792b7b
VoodooPrograms <arkanidius@gmail.com>
2021-05-17 21:31:16 +0200

    Add sidebar component with browse page

--- ui/ui/src/App.vue
+++ ui/ui/src/App.vue
@@ -33,7 +33,6 @@ main {
   left: 0;
   width: 100vw;
   height: 100vh;
-  overflow: hidden;
 }
 
 :root {
---
+++ ui/ui/src/views/Browse.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="grid-container">
+    <Navigation></Navigation>
+    <div class="col-10">
+      <RadioCard></RadioCard>
+      <RadioCard></RadioCard>
+      <RadioCard></RadioCard>
+      <RadioCard></RadioCard>
+      <RadioCard></RadioCard>
+      <RadioCard></RadioCard>
+      <RadioCard></RadioCard>
+      <RadioCard></RadioCard>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Navigation from "@/components/Navigation";
+import RadioCard from "@/components/RadioCard";
+
+export default {
+  name: "Browse",
+  components: {RadioCard , Navigation}
+}
+</script>
+
+<style scoped>
+.grid-container {
+  width: 100%;
+  height: 100vh;
+}
+
+.col-10 {
+  background-color: black;
+  width: 84%;
+  float: right;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+@media only screen and (max-width: 768px) {
+  .col-10 {
+    width: 100%;
+  }
+}
+</style>
